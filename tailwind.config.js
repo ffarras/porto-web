447f0c004f7ce56db7813d3be6de7f25eaee3381
faishal <faishalfarras.bts@gmail.com>
2024-02-12 20:25:59 +0700

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: ["index.html", "./static/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["InterVariable", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
