7440be46a59bd1c034855c595071428156479184
faishal <faishalfarras.bts@gmail.com>
2024-02-13 16:59:24 +0700

    add content, adjust layout for multiple breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["index.html", "./static/*.html"],
   theme: {
+    screens: {
+      'xs': '200px',
+      ...defaultTheme.screens,
+    },
     extend: {
       fontFamily: {
         sans: ["InterVariable", ...defaultTheme.fontFamily.sans],
